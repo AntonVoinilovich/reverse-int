7e7b6df6d44e7d9d79c775f7ebd03e0db3f24f9f
AntonVoinilovich <anton.voinilovich@gmail.com>
2021-03-16 01:44:03 +0300

    fix: correct code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function reverse (n) {
     let string = String(Math.abs(n));
     let stringReverse = "";
     for (let i = string.length - 1 ; i >= 0 ; i--) {
-        str2 += string[i];
+        stringReverse += string[i];
     }
     return Number(stringReverse);
 }
